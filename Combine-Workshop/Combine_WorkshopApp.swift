22d631399be5209bfee84a070c6849626d585c41
Manish <manish.patidar.97540@gmail.com>
2022-03-23 16:22:43 +0530

    Initial Commit

---
+++ Combine-Workshop/Combine_WorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Combine_WorkshopApp.swift
+//  Combine-Workshop
+//
+//  Created by Manish Patidar on 23/03/22.
+//
+
+import SwiftUI
+
+@main
+struct Combine_WorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
